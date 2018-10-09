8719997676ab3fe610bdc0195e4a5bcd767aab76
Ben Rosner <br305893@gmail.com>
2018-10-09 00:38:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {
-  Grateful Dead: "Morning Dew",
-  Phish: "Harry Hood",
-  The Allman Brothers Band: 'Whipping Post'
+  gratefulDead: "Morning Dew",
+  phish: "Harry Hood",
+  theAllmanBrothersBand: 'Whipping Post'
 }
