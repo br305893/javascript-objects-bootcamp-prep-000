86e64f0acd696802f52fcd0ab74a4b4aa59e8ebd
Ben Rosner <br305893@gmail.com>
2018-10-09 00:41:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,6 @@ var playlist = {
   phish: "Harry Hood",
   theAllmanBrothersBand: 'Whipping Post'
 }
+
+function updatePlaylist(playlist, artistName, songTitle)
+ return Object.assign({}, playlist,{[moe]: "Rebubula"})
