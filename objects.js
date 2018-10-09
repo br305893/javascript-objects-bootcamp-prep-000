865a26112640cffffc809783efd640b0f683c7bc
Ben Rosner <br305893@gmail.com>
2018-10-09 01:05:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
- Object.assign({}, playlist,{['Widespread Panic']: "Chilly Water"})
+ Object.assign({}, playlist,{artistName: songTitle})
  return playlist
 }
