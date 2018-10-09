56137b29df3753cade6e480aabda8daa747674c4
Ben Rosner <br305893@gmail.com>
2018-10-09 00:59:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {
-  'Grateful Dead': "Morning Dew",
-  'Phish': "Harry Hood",
-  'The Allman Brothers Band': 'Whipping Post'
+  ['Grateful Dead']: "Morning Dew",
+  ['Phish']: "Harry Hood",
+  ['The Allman Brothers Band']: 'Whipping Post'
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
  Object.assign({}, playlist,{['Widespread Panic']: "Chilly Water"})
- return updatePlaylist}
+ return playlist}
