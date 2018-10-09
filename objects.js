7060e18b76aedb8a66348596cb25bf641c0029fc
Ben Rosner <br305893@gmail.com>
2018-10-09 01:24:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
  playlist[artistName] = songTitle
 return playlist}
 
-function removePlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
 return playlist}
